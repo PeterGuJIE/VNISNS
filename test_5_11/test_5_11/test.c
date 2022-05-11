6adfbff34d082649c9ca7458524c57ed0e213d32
PeterGUJIE <783814087@qq.com>
2022-05-11 18:54:13 +0800

    test_5_11

---
+++ test_5_11/test_5_11/test.c
@@ -0,0 +1,123 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+void GetMemory(char**p) 
+{
+	*p = (char *)malloc(100);
+}
+void Test(void) {
+	char *str = NULL;
+	GetMemory(&str);
+	strcpy(str, "hello world");
+	printf(str);
+	free(str);
+	str = NULL;
+}
+int main()
+{
+	Test();
+	return 0;
+}
+
+
+
+
+
+
+//int main()
+//{
+//	int*p = (int*)malloc(30);
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for (i = 0; i < 5; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//	}
+//	int*ptr = realloc(p, 4000);
+//	if (ptr != NULL)
+//	{
+//		p = ptr;
+//		int i = 0;
+//		for (i = 5; i < 10; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+
+
+
+
+
+
+//int main()
+//{
+//	int*p = (int*)calloc(10, sizeof(int));
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+
+
+
+
+
+
+//struct s
+//{
+//	char name[20];
+//	int age;
+//};
+//int main()
+//{
+//	int*p = (int*)malloc(10 * sizeof(int));
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for (i = 0; i < 10; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	free(p);
+//	p = NULL;
+//	/*struct s arr[50];*/
+//	return 0;
+//}
