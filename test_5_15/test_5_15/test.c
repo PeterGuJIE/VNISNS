b5396f11bcb606d0984931d60f3e2bf191544921
PeterGUJIE <783814087@qq.com>
2022-05-15 21:12:10 +0800

    test_5_15

---
+++ test_5_15/test_5_15/test.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#define MAX 100
+//int main()
+//{
+//	printf("%d\n", MAX);
+//#undef MAX
+//	return 0;
+//}
+
+
+
+
+
+//#define SIZEOF(type) sizeof(type)
+//#define MALLOC(num,type) (type*)malloc(num*sizeof(type))
+//int main()
+//{
+//	int ret = SIZEOF(int);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+
+
+//#define MAX(X,Y) (X)>(Y)?(X):(Y)
+//int main()
+//{
+//	int a = 10;
+//	int b = 11;
+//	int max = MAX(a++, b++);
+//	printf("%d\n", max);
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 10;
+//	int b = a + 1;
+//	int b = ++a;
+//	return 0;
+//}
+
+
+
+//#define CAT(X,Y) X##Y
+//int main()
+//{
+//	int Class84 = 2019;
+//	printf("%d\n", CAT(Class, 84));
+//	return 0;
+//}
+
+
+
+
+
+//#define PRINT(X) printf("the value of "#X" is %d\n",X)
+////void print(int a)
+////{
+////	printf("the value of a is %d\n", a);
+////}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	PRINT(a);
+//	PRINT(b);
+//	return 0;
+//}
+
+
+
+
+//#define SQUARE(x) x*x
+//int main()
+//{
+//	int ret = SQUARE(5);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	printf("%s\n", __FILE__);
+//	printf("%d\n", __LINE__);
+//	printf("%d\n", __DATE__);
+//	printf("%d\n", __TIME__);
+//	return 0;
+//}
